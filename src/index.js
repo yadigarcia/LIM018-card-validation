77a03f7165c79d8810dc1f3e91584863921af666
Yadira Garcia <leyadve@gmail.com>
2022-06-13 07:37:41 -0500

    13/06/2022 casi listo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import validator from './validator.js';
+//console.log(validator);
 
-console.log(validator);
+let creditCardNumber = document.getElementById("creditCardNumber");
+let btnPay = document.getElementById("btnPay");
+
+btnPay.addEventListener("click",(e) => {
+    e.preventDefault();
+    
+let algLunh = validator.isValid(creditCardNumber.value);
+creditCardNumber.value = validator.maskify(creditCardNumber.value);
+   if(algLunh === true){
+           alert("Procesando Pago")
+    } else {
+            alert("Tarjeta Invalida, Ingrese otra tarjeta");
+    }
+    
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,42 @@
 const validator = {
   // ...
+
+isValid: function (creditCardNumber) {
+
+    const numberReverse =creditCardNumber.toString().split("").reverse().join("");
+    let addNumber = 0 ;
+    
+
+     for (let i = 0; i < numberReverse.length; i++){
+      let digit=parseInt(numberReverse[i]);
+         if(i % 2 == 0){
+           if((digit *=2) > 9){
+             digit -= 9;
+           }
+         }
+          addNumber += digit;
+          }
+        return addNumber % 10 === 0 ;
+     }
+    
+ , maskify: function(creditCardNumber){
+    let encriptNumber;//creditcardNumber con #
+    let newArrayNumber= [];
+
+    //separrar array
+    let  arrayNumber = creditCardNumber.split("");
+      
+     arrayNumber.forEach((element, i) => { 
+      if (i < arrayNumber.length - 4 ){ 
+        element = "#";
+      }
+      
+     // utilizar push para agregar digit al array vacio
+      newArrayNumber.push(element);
+      encriptNumber= arrayNumber.join("");
+    });
+   return encriptNumber;
+  }
 };
 
 export default validator;
